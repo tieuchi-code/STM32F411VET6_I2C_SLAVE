4eaab3542f236cd35db691677642362baf6d8fd2
tieuchi-code <tieuchi2005@gmail.com>
2025-06-17 19:55:10 +0700

    write funtion

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,236 +1,93 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+#define GPIOA_BASE_ADDR	0x40020000
+#define GPIOB_BASE_ADDR	0x40020400
+#define GPIOC_BASE_ADDR	0x40020800
+#define GPIOD_BASE_ADDR 0x40020C00
+#define GPIOE_BASE_ADDR 0x40021000
+#define GPIOH_BASE_ADDR 0x40021C00
 
-/* USER CODE END Includes */
+#define USART1_BASE_ADDR 0x40011000
+#define I2C1_BASE_ADDR 0x40005400
+#define SPI1_BASE_ADDR 0x40013000
+#define I2S1_BASE_ADDR 0x40013000
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+#define DMA1_BASE_ADDR 0x40026000
 
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-uint8_t data;
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
+void I2C1_Slave_Init()	//PB6,PB7	SLAVE
 {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_I2C1_CLK_ENABLE();
 
-  /* USER CODE BEGIN 1 */
+    // MODER: Alternate function (10)
+    volatile uint32_t* MODER   = (uint32_t*)(GPIOB_BASE_ADDR + 0x00);
+     *MODER &= ~((0b11 << (6 * 2)) | (0b11 << (7 * 2)));
+     *MODER |=  ((0b10 << (6 * 2)) | (0b10 << (7 * 2)));
 
-  /* USER CODE END 1 */
+     // AFRL: AF4 for I2C
+     volatile uint32_t* AFRL    = (uint32_t*)(GPIOB_BASE_ADDR + 0x20);
+     *AFRL &= ~((0xF << (6 * 4)) | (0xF << (7 * 4)));
+     *AFRL |=  ((0x4 << (6 * 4)) | (0x4 << (7 * 4)));
 
-  /* MCU Configuration--------------------------------------------------------*/
+     // OTYPER: Open-drain
+     volatile uint32_t* OTYPER  = (uint32_t*)(GPIOB_BASE_ADDR + 0x04);
+     *OTYPER |= (1 << 6) | (1 << 7);
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+     // PUPDR: Pull-up
+     volatile uint32_t* PUPDR   = (uint32_t*)(GPIOB_BASE_ADDR + 0x0C);
+     *PUPDR &= ~((0b11 << (6 * 2)) | (0b11 << (7 * 2)));
+     *PUPDR |=  ((0b01 << (6 * 2)) | (0b01 << (7 * 2)));
 
-  /* USER CODE BEGIN Init */
+     // OSPEEDR: High speed
+     volatile uint32_t* OSPEEDR = (uint32_t*)(GPIOB_BASE_ADDR + 0x08);
+     *OSPEEDR &= ~((0b11 << (6 * 2)) | (0b11 << (7 * 2)));
+     *OSPEEDR |= (0b11 << (6 * 2)) | (0b11 << (7 * 2));
 
-  /* USER CODE END Init */
+     //Cấu hình I2C1
+     volatile uint32_t* CR1   = (uint32_t*)(I2C1_BASE_ADDR + 0x00);
+     *CR1 &= ~(1 << 0);     // Disable I2C (PE = 0)
 
-  /* Configure the system clock */
-  SystemClock_Config();
+     *CR1 |= (1 << 10); // Bật ACK
 
-  /* USER CODE BEGIN SysInit */
+     //Cấu hình địa chỉ Slave
+     volatile uint32_t* OAR1   = (uint32_t*)(I2C1_BASE_ADDR + 0x08);
+     *OAR1 |= (1 << 14);	//luôn giữ = 1
+     *OAR1 &= ~(1 << 15);	// 7 bit address
+     *OAR1 &= ~(0b1111111 << 1);	//Clear
+     *OAR1 |= (0x42 << 1);			//Địa chỉ 0x42
 
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  HAL_I2C_Slave_Receive(&hi2c1, &data, 1, HAL_MAX_DELAY);
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+     *CR1 |= (1 << 0);      // Enable I2C
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+uint8_t I2C1_Slave_Receive(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    volatile uint32_t* CR1 = (uint32_t*)(I2C1_BASE_ADDR + 0x00);
+    volatile uint32_t* SR1 = (uint32_t*)(I2C1_BASE_ADDR + 0x14);
+    volatile uint32_t* SR2 = (uint32_t*)(I2C1_BASE_ADDR + 0x18);
+    volatile uint32_t* DR  = (uint32_t*)(I2C1_BASE_ADDR + 0x10);
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    // 1. Đợi Master
+    while (!(*SR1 & (1 << 1)));  // ADDR = 1
+    (void)*SR2;                  // clear cờ ADDR
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    // 2. Đợi dữ liệu
+    while (!(*SR1 & (1 << 6)));	//RxNE = 1
+    uint8_t received_data = *DR;
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    // Đợi Master gửi STOP
+    while (!(*SR1 & (1 << 4)));   // STOPF = 1
+    *CR1 |= (1 << 0);             // Clear STOPF bằng cách ghi lại PE
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    return received_data;
 }
 
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
+int main()
 {
+	HAL_Init();
+	I2C1_Slave_Init();
+	I2C1_Slave_Receive();
+	while(1)
+	{
 
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 132;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
+	}
 }
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
